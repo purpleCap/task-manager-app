12266bda116504f258bdb280ee6113c00851a279
Pratik Saha <pratik.saha14@gmail.com>
2025-02-25 00:18:03 +0530

    env config added

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ["module:react-native-dotenv", {
+      "envName": "REACT_APP_ENV",
+      "moduleName": "@env",
+      "path": ".env",
+      "safe": false,
+      "allowUndefined": true,
+      "verbose": false
+    }]
+  ],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
